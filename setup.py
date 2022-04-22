e858f13593e3ab5e67f9422800459891fa8b7289
YodaPY <yodarlswitch@gmail.com>
2022-04-22 16:56:51 +0200

    0.0.3, fix setup.py

--- setup.py
+++ setup.py
@@ -58,7 +58,7 @@ setup(
         "Natural Language :: English",
         "Topic :: Software Development :: Libraries :: Python Modules",
         "Programming Language :: Python :: 3.6",
-        "Programming Language :: Python :: 3.7"
+        "Programming Language :: Python :: 3.7",
         "Programming Language :: Python :: 3.8",
         "Programming Language :: Python :: 3.9",
         "Programming Language :: Python :: 3.10",
